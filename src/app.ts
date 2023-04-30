ed5d899eac9938413b0a8b6e6bcd8f8252eca893
AndersonKeller <andersonkellervendas@hotmail.com>
2023-04-30 13:41:43 -0300

    remove cors

--- src/app.ts
+++ src/app.ts
@@ -3,17 +3,17 @@ import "express-async-errors";
 import { handleErrors } from "./errors";
 import { loginRoute } from "./routers/login.routes";
 import { userRoutes } from "./routers/users.routes";
-const cors = require("cors");
+// const cors = require("cors");
 const app: Application = express();
 app.use(express.json());
-app.use((req, res, next) => {
-  //Qual site tem permissão de realizar a conexão, no exemplo abaixo está o "*" indicando que qualquer site pode fazer a conexão
-  res.header("Access-Control-Allow-Origin", "*");
-  //Quais são os métodos que a conexão pode realizar na API
-  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
-  app.use(cors());
-  next();
-});
+// app.use((req, res, next) => {
+//   //Qual site tem permissão de realizar a conexão, no exemplo abaixo está o "*" indicando que qualquer site pode fazer a conexão
+//   res.header("Access-Control-Allow-Origin", "*");
+//   //Quais são os métodos que a conexão pode realizar na API
+//   res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
+//   app.use(cors());
+//   next();
+// });
 app.use("/users", userRoutes);
 app.use("/login", loginRoute);
 
